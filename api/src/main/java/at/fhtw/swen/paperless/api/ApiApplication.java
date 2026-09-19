a19082ebd7e7c3c44a8ba58ebcf0c3a3ad7e340d
Wenhao <if24b216@technikum-wien.at>
2026-09-19 11:48:06 +0200

    Set up Spring boot application in api module

---
+++ api/src/main/java/at/fhtw/swen/paperless/api/ApiApplication.java
@@ -0,0 +1,12 @@
+package at.fhtw.swen.paperless.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
