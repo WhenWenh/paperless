411cd29223c9481cd81bb1ab1f799715bbf94b97
wenhao <if24b216@technikum-wien.at>
2026-09-20 14:43:54 +0200

    added document Request and first db schema

---
+++ api/src/main/java/at/fhtw/swen/paperless/api/controller/request/CreateDocumentRequest.java
@@ -0,0 +1,27 @@
+package at.fhtw.swen.paperless.api.controller.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
+public record CreateDocumentRequest(
+
+        @NotBlank
+        @Size(max = 255)
+        String title,
+
+        @NotBlank
+        @Size(max = 512)
+        String originalFilename,
+
+        @NotBlank
+        @Size(max = 127)
+        String contentType,
+
+        @NotNull
+        @PositiveOrZero
+        Long fileSize
+
+) {
+}
